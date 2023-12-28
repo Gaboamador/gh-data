442a06fa8ec7fcca6489d905c2082076bc9a1927
Gaboamador <119951076+Gaboamador@users.noreply.github.com>
2023-12-28 17:50:45 -0300

    Add files via upload

---
+++ participantsData.js
@@ -0,0 +1,8 @@
+export const participants = [
+"Agostina","Alan","Bautista","Carla","Catalina","Denisse","Emmanuel","Federico","Florencia","Isabel","Joel","Juliana","Lisandro","Lucía","Martín","Nicolás","Rosina","Sabrina","Williams","Zoe"
+];
+
+
+export const participantsChart = [
+    "Agostina","Alan","Axel","Bautista","Carla","Catalina","Denisse","Emmanuel","Federico","Florencia","Hernán","Isabel","Joel","Juliana","Lisandro","Lucía","Martín","Nicolás","Rosina","Sabrina","Williams","Zoe"
+    ];
