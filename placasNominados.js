e5d1963a38f0784b96186f9f0d050a28fe6f85a1
Gaboamador <119951076+Gaboamador@users.noreply.github.com>
2023-12-28 18:24:21 -0300

    Update placasNominados.js

--- placasNominados.js
+++ placasNominados.js
@@ -1,36 +1,36 @@
 [
-{week: 0,
-    data: [  
-      { role: 'Líder', name: 'Sabrina', result: '-' },
-      { role: 'Nominado', name: 'Catalina', result: 'Salvado por el líder' },
-      { role: 'Nominado', name: 'Zoe', result: '10,60% (Entre 4)' },
-      { role: 'Nominado por el líder', name: 'Williams', result: '6,76% (Entre 3)' },
-      { role: 'Nominado', name: 'Juliana', result: '47,40% (Entre 2)' },
-      { role: 'Nominado', name: 'Hernán', result: 'Eliminado (52,60%)' },
-    ],
-    },
-    {week: 1,
-      data: [  
-        { role: 'Líder', name: 'Martín', result: '-' },
-        { role: 'Nominado por el líder', name: 'Agostina', result: '2,90% (Entre 5)' },
-        { role: 'Nominado', name: 'Carla', result: '20,90% (Entre 4)' },
-        { role: 'Nominado', name: 'Catalina', result: '32,50% (Entre 3)' },
-        { role: 'Nominado', name: 'Juliana', result: '45,70% (Entre 2)' },
-        { role: 'Nominado', name: 'Axel', result: 'Eliminado (54,30%)' },
-        
-      ],
-      },
-      {week: 2,
-        data: [  
-          { role: 'Líder', name: 'Martín', result: '-' },
-          // { role: 'Nominado por el líder', name: '', result: '' },
-          { role: 'Nominado', name: 'Juliana', result: '' },
-          { role: 'Nominado', name: 'Williams', result: '' },
-          { role: 'Nominado', name: 'Florencia', result: '' },
-          { role: 'Nominado', name: 'Carla', result: '' },
-          { role: 'Nominado', name: 'Sabrina', result: '' },
-          { role: 'Nominado', name: 'Lisandro', result: '' },
-          
-        ],
-        },
- ];
+  {
+    "week": 0,
+    "data": [
+      { "role": "Líder", "name": "Sabrina", "result": "-" },
+      { "role": "Nominado", "name": "Catalina", "result": "Salvado por el líder" },
+      { "role": "Nominado", "name": "Zoe", "result": "10,60% (Entre 4)" },
+      { "role": "Nominado por el líder", "name": "Williams", "result": "6,76% (Entre 3)" },
+      { "role": "Nominado", "name": "Juliana", "result": "47,40% (Entre 2)" },
+      { "role": "Nominado", "name": "Hernán", "result": "Eliminado (52,60%)" }
+    ]
+  },
+  {
+    "week": 1,
+    "data": [
+      { "role": "Líder", "name": "Martín", "result": "-" },
+      { "role": "Nominado por el líder", "name": "Agostina", "result": "2,90% (Entre 5)" },
+      { "role": "Nominado", "name": "Carla", "result": "20,90% (Entre 4)" },
+      { "role": "Nominado", "name": "Catalina", "result": "32,50% (Entre 3)" },
+      { "role": "Nominado", "name": "Juliana", "result": "45,70% (Entre 2)" },
+      { "role": "Nominado", "name": "Axel", "result": "Eliminado (54,30%)" }
+    ]
+  },
+  {
+    "week": 2,
+    "data": [
+      { "role": "Líder", "name": "Martín", "result": "-" },
+      { "role": "Nominado", "name": "Juliana", "result": "" },
+      { "role": "Nominado", "name": "Williams", "result": "" },
+      { "role": "Nominado", "name": "Florencia", "result": "" },
+      { "role": "Nominado", "name": "Carla", "result": "" },
+      { "role": "Nominado", "name": "Sabrina", "result": "" },
+      { "role": "Nominado", "name": "Lisandro", "result": "" }
+    ]
+  }
+]
