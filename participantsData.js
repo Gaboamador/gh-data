32018ff92fb3c40bfbb4beb9a140faeffdd6b311
Gaboamador <119951076+Gaboamador@users.noreply.github.com>
2024-01-16 17:10:06 -0300

    Delete participantsData.js

--- participantsData.js
+++
@@ -1,8 +0,0 @@
-export const participants = [
-"Agostina","Alan","Bautista","Carla","Catalina","Denisse","Emmanuel","Federico","Florencia","Isabel","Joel","Juliana","Lisandro","Lucía","Martín","Nicolás","Rosina","Sabrina","Williams","Zoe"
-];
-
-
-export const participantsChart = [
-    "Agostina","Alan","Axel","Bautista","Carla","Catalina","Denisse","Emmanuel","Federico","Florencia","Hernán","Isabel","Joel","Juliana","Lisandro","Lucía","Martín","Nicolás","Rosina","Sabrina","Williams","Zoe"
-    ];
